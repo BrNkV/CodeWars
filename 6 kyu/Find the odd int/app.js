4f496bcb9cde5b26a3107984dd6801da50a7b478
EveryBarry <49118930+EveryBarry@users.noreply.github.com>
2021-09-15 11:55:10 +0300

    Find the odd int

--- 6 kyu/Find the odd int/app.js
+++ 6 kyu/Find the odd int/app.js
@@ -1,3 +1,6 @@
+// Find the odd int
+//https://www.codewars.com/kata/54da5a58ea159efa38000836/javascript
+
 /**
  * Given an array of integers, find the one that appears an odd number of times.
 
@@ -22,9 +25,57 @@ describe('Example tests', function() {
 });
  */
 
-function findOdd(A) {
-    //happy coding!
-    return 0;
-  }
+// мое решение 0.169 seconds
+function findOdd(arr) {
+  //happy coding!
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        count++;
+      }
+    }
+    if (count % 2 != 0) {
+      console.log(arr[i]);
+      return arr[i];
+    }
+  };
+}
 
-  
+
+let arr = [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5];
+let arr1 = [1,1,2,-2,5,2,4,4,-1,-2,5];
+let arr2 =[20,1,1,2,2,3,3,5,5,4,20,4,5];
+let arr3 = [10];
+let arr4 = [1,1,1,1,1,1,10,1,1,1,1];
+let arr5 = [5,4,3,2,1,5,4,3,2,10,10];
+
+// console.log(findOdd(arr));
+// console.log(findOdd(arr1));
+// console.log(findOdd(arr2));
+// console.log(findOdd(arr3));
+
+findOdd(arr);
+findOdd(arr1);
+findOdd(arr2);
+findOdd(arr3);
+findOdd(arr4);
+findOdd(arr5);
+
+// решения которые понравились:
+
+/*1
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+*/
+
+/*2
+function findOdd(A) {
+  return A.reduce(function(c,v){return c^v;},0);
+}
+*/
+
+/*3
+function findOdd(arr) {
+  return arr.find((item, index) => arr.filter(el => el == item).length % 2)
+}
+*/
