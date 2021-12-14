f8cf156f6d62928a15709805df845594fd246ab1
EveryBarry <49118930+EveryBarry@users.noreply.github.com>
2021-12-14 23:52:49 +0300

    20 easy lvl

--- 7-8 kyu/Codewars Easy/20 Switch it Up.js
+++ 7-8 kyu/Codewars Easy/20 Switch it Up.js
@@ -1,4 +1,4 @@
-/* TODO
+/* Complete
 
 When provided with a number between 0-9, return it in words.
 
@@ -35,17 +35,33 @@ switch (expression) {
 
 */
 
-function switchItUp(number){
-    switch (number >= 0 && number <= 9) {
-      case 'One':
-        number == 1;
-        break;
-    
-      default:
-        break;
-    }
-    }
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return 'Zero';
+    case 1:
+      return 'One';
+    case 2:
+      return 'Two';
+    case 3:
+      return 'Three';
+    case 4:
+      return 'Four';
+    case 5:
+      return 'Five';
+    case 6:
+      return 'Six';
+    case 7:
+      return 'Seven';
+    case 8:
+      return 'Eight';
+    case 9:
+      return 'Nine';
+    default:
+      break;
+  }
+}
 
-  console.log((switchItUp(1)));//,"One");
-  console.log((switchItUp(3)));//,"Three");
-  console.log((switchItUp(5)));//,"Five");
+console.log((switchItUp(1)));//,"One");
+console.log((switchItUp(3)));//,"Three");
+console.log((switchItUp(5)));//,"Five");
