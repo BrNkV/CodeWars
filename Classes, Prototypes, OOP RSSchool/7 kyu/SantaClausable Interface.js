0c74188f57795ea54c275a2ba2881c51108b5465
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2022-06-15 16:25:11 +0300

    feat: santa

--- Classes, Prototypes, OOP RSSchool/7 kyu/SantaClausable Interface.js
+++ Classes, Prototypes, OOP RSSchool/7 kyu/SantaClausable Interface.js
@@ -1,45 +1,35 @@
-// function isSantaClausable(obj) {
-//     class Santa {
-//         constructor(name) {
-//             this.name = obj;
-//         }
-//         sayHoHoHo() { console.log('Ho Ho Ho!') }
-//         distributeGifts() { console.log('Gifts for all!'); }
-//         goDownTheChimney() { console.log('*whoosh*'); }
-//     }
+const santa = {
+    sayHoHoHo: function () { console.log('Ho Ho Ho!') },
+    distributeGifts: function () { console.log('Gifts for all!'); },
+    goDownTheChimney: function () { console.log('*whoosh*'); }
+};
 
-//     class NotSanta {
-//         constructor(name) {
-//             this.name = obj;
-//         }
-//         sayHoHoHo() { console.log('Oink Oink!') }
-//         // no distributeGifts() and no goDownTheChimney()
-//     };
+const notSanta = {
+    sayHoHoHo: function () { console.log('Oink Oink!') }
+};
 
-//     let santa = new Santa();
-//     let notSanta = new NotSanta();
-
-//     console.log(santa)
-//     console.log(notSanta)
-
-// };
-
-// const santa = {
-//     sayHoHoHo: function () { console.log('Ho Ho Ho!') },
-//     distributeGifts: function () { console.log('Gifts for all!'); },
-//     goDownTheChimney: function () { console.log('*whoosh*'); }
-// };
-
-// const notSanta = {
-//     sayHoHoHo: function () { console.log('Oink Oink!') }
-// };
-
-// console.log(isSantaClausable(santa));
-// console.log(isSantaClausable(notSanta));
-
-// // isSantaClausable(santa);
-// // isSantaClausable(notSanta);
+console.log(isSantaClausable(santa));
+console.log(isSantaClausable(notSanta));
 
 function isSantaClausable(obj) {
-
+    if (obj.sayHoHoHo && typeof obj.sayHoHoHo == 'function'
+        && obj.distributeGifts && typeof obj.distributeGifts == 'function'
+        && obj.goDownTheChimney && typeof obj.goDownTheChimney == 'function') return true;
+    else return false;
 }
+
+
+/* best answer
+1
+function isSantaClausable(obj) {
+  return ['sayHoHoHo', 'distributeGifts', 'goDownTheChimney'].every(function(methodName) {
+    return typeof obj[methodName] == 'function';
+  });
+}
+
+2
+function isSantaClausable(obj) {
+  return 'function' == typeof (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney);
+}
+
+*/
