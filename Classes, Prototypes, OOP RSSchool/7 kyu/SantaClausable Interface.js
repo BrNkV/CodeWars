684001332004291bae6286cf683f782bf9bb5f99
Igor Burenkov <burenkov.igor@gmail.com>
2022-06-15 04:56:58 +0300

    feat: start

---
+++ Classes, Prototypes, OOP RSSchool/7 kyu/SantaClausable Interface.js
@@ -0,0 +1,45 @@
+// function isSantaClausable(obj) {
+//     class Santa {
+//         constructor(name) {
+//             this.name = obj;
+//         }
+//         sayHoHoHo() { console.log('Ho Ho Ho!') }
+//         distributeGifts() { console.log('Gifts for all!'); }
+//         goDownTheChimney() { console.log('*whoosh*'); }
+//     }
+
+//     class NotSanta {
+//         constructor(name) {
+//             this.name = obj;
+//         }
+//         sayHoHoHo() { console.log('Oink Oink!') }
+//         // no distributeGifts() and no goDownTheChimney()
+//     };
+
+//     let santa = new Santa();
+//     let notSanta = new NotSanta();
+
+//     console.log(santa)
+//     console.log(notSanta)
+
+// };
+
+// const santa = {
+//     sayHoHoHo: function () { console.log('Ho Ho Ho!') },
+//     distributeGifts: function () { console.log('Gifts for all!'); },
+//     goDownTheChimney: function () { console.log('*whoosh*'); }
+// };
+
+// const notSanta = {
+//     sayHoHoHo: function () { console.log('Oink Oink!') }
+// };
+
+// console.log(isSantaClausable(santa));
+// console.log(isSantaClausable(notSanta));
+
+// // isSantaClausable(santa);
+// // isSantaClausable(notSanta);
+
+function isSantaClausable(obj) {
+
+}
